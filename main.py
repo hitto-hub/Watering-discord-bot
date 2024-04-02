541653c532ccecb7dfc31f75ede38615cb562033
hittowsl <68594072+hitto-hub@users.noreply.github.com>
2024-04-02 12:19:34 +0900

    fix: エラーメッセージ関連

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ token = str(os.getenv("TOKEN"))
 url = str(os.getenv("API_URL"))
 
 # channelid
-# val
-valchannelid = str(os.getenv("VAL_CHANNEL_ID"))
-# notice
-noticechannelid = str(os.getenv("NOTICE_CHANNEL_ID"))
+Valchannelid = str(os.getenv("VAL_CHANNEL_ID"))
+Noticechannelid = str(os.getenv("NOTICE_CHANNEL_ID"))
+Logchannelid = str(os.getenv("LOG_CHANNEL_ID"))
+
+Valchannel = None
+Noticechannel = None
+Logchannel = None
 
 num_val = 0
 num_notice = 0
@@ -37,27 +40,41 @@ bot = discord.Bot(
 
 wateringregular = bot.create_group(name="wateringregular", description="水やり予約関連")
 
+def makelog(header, message):
+    return f'[{time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())} : {header}] {message}'
+
 # 起動時に自動的に動くメソッド
 @bot.event
 async def on_ready():
     global num_val
     global num_notice
-    # 起動すると、実行したターミナルに"Hello!"と表示される
-    print("Hello!")
+    global Valchannel
+    global Noticechannel
+    global Logchannel
+    # channelの取得
     for channel in bot.get_all_channels():
-        if int(channel.id) == int(valchannelid):
-            await channel.send(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()) + ":起動しました\n")
-    # Message
-    # notice id
+        if int(channel.id) == int(Valchannelid):
+            Valchannel = channel
+            print("Valchannelを取得しました")
+        if int(channel.id) == int(Noticechannelid):
+            Noticechannel = channel
+            print("Noticechannelを取得しました")
+        if int(channel.id) == int(Logchannelid):
+            Logchannel = channel
+            print("Logchannelを取得しました")
+    if Valchannel is None or Noticechannel is None or Logchannel is None:
+        print("Error: channelの取得に失敗しました。チャンネルIDを確認してください。")
+        exit()
     try:
         num_val = int(json.loads(requests.get(url + "/val").text)["num_results"])
         num_notice = int(json.loads(requests.get(url + "/notice").text)["num_results"])
     except:
-        errmes = "Error: メッセージの取得に失敗しました"
-        # メッセージの取得に失敗した場合、エラーメッセージを送信
-        for channel in bot.get_all_channels():
-            if int(channel.id) == int(valchannelid):
-                await channel.send(errmes)
+        mes = "メッセージの取得に失敗しました。apiサーバーが起動しているか確認してください。"
+        await Logchannel.send(makelog("Error", mes))
+        exit()
+    # Logchannelにメッセージを送信
+    mes = "正常に起動しました\n"
+    await Logchannel.send(makelog("Info", mes))
 
 # pingコマンドを実装
 @bot.command(name="ping", description="pingを返します")
@@ -72,11 +89,15 @@ async def watering(ctx: discord.ApplicationContext):
     try:
         response = requests.post(flag_url, json=data)
     except:
-        await ctx.respond(f"Error: 水やり指示の送信に失敗しました")
+        mes = "水やり指示の送信に失敗しました, apiサーバーが起動しているか確認してください"
+        await ctx.respond(f"Error: {mes}")
+        await Logchannel.send(makelog("Error", mes))
         return
     # print(response.status_code)
     # print(response.text)
-    await ctx.respond(f"水やり指示を出しました")
+    mes = "水やり指示を出しました"
+    await ctx.respond(f"{mes}")
+    await Logchannel.send(makelog("Info", mes))
 
 @wateringregular.command(
     name="add",
@@ -86,10 +107,14 @@ async def add(ctx: discord.ApplicationContext,
                 settime: discord.Option(str, "時間\"HH:MM\"を入力してください", name="time"),
                 weekday: discord.Option(str, "曜日を入力してください", name="weekday", choices=["mon", "tue", "wed", "thu", "fri", "sat", "sun", "all"])):
     if settime + " " + weekday in watering_time:
-        await ctx.respond(f"水やり予約が重複しています\n{watering_time}")
+        mes = "水やり予約が重複しています"
+        await ctx.respond(mes)
+        await Logchannel.send(makelog("Error", mes))
     else:
         watering_time.add(settime + " " + weekday)
-        await ctx.respond(f"水やり予約を追加しました\n{watering_time}")
+        mes = f"水やり予約を追加しました。{watering_time}"
+        await ctx.respond(mes)
+        await Logchannel.send(makelog("Info", mes))
 
 @wateringregular.command(
     name="remove",
@@ -101,11 +126,17 @@ async def remove(ctx: discord.ApplicationContext,
     if settime + " " + weekday in watering_time:
         watering_time.discard(settime + " " + weekday)
         if watering_time == set():
-            await ctx.respond(f"水やり予約を全て削除しました")
+            mes = f"水やり予約を全て削除しました。{watering_time}"
+            await ctx.respond(mes)
+            await Logchannel.send(makelog("Info", mes))
         else:
-            await ctx.respond(f"水やり予約を削除しました\n{watering_time}")
+            mes = f"水やり予約を削除しました。{watering_time}"
+            await ctx.respond(mes)
+            await Logchannel.send(makelog("Info", mes))
     else:
-        await ctx.respond(f"水やり予約が見つかりませんでした")
+        mes = f"指定した水やり予約({settime} {weekday})が見つかりませんでした。"
+        await ctx.respond(mes)
+        await Logchannel.send(makelog("Error", mes))
 
 @wateringregular.command(
     name="list",
@@ -113,9 +144,13 @@ async def remove(ctx: discord.ApplicationContext,
 )
 async def list(ctx: discord.ApplicationContext):
     if len(watering_time) == 0:
-        await ctx.respond(f"水やり予約はありません")
+        mes = "水やり予約はありません"
+        await ctx.respond(mes)
+        await Logchannel.send(makelog("Info", mes))
     else:
-        await ctx.respond(f"水やり予約一覧\n{watering_time}")
+        mes = f"水やり予約一覧\n{watering_time}"
+        await ctx.respond(mes)
+        await Logchannel.send(makelog("Info", mes))
 
 # 10秒ごとにchannelidにメッセージを送信
 # ToDo: 値の取得、表示方法を改善
@@ -129,27 +164,25 @@ async def get_val():
         response = requests.get(url + "/val")
         data = json.loads(response.text)
     except:
-        errmes = "Error: メッセージの取得に失敗しました"
-        # メッセージの取得に失敗した場合、エラーメッセージを送信
-        for channel in bot.get_all_channels():
-            if int(channel.id) == int(valchannelid):
-                await channel.send(errmes)
+        mes = "メッセージの取得に失敗しました. apiサーバーが起動しているか確認してください.val"
+        await Valchannel.send(makelog("Error", mes))
+        await Logchannel.send(makelog("Error", mes))
         return
     # channel指定 要改善
-    for channel in bot.get_all_channels():
-        if int(channel.id) == int(valchannelid):
-            for entry in data["data"]:
-                # Message[id]が存在しない場合、Message[id]に格納
-                if num_val < int(entry['id']):
-                    num_val = int(entry['id'])
-                    mes = f"{entry['timestamp']} : {entry['val']}\n"
-                    await channel.send(mes)
-            # print(num_val, len(data["data"]))
-            if num_val > len(data["data"]):
-                num_val = int(json.loads(requests.get(url + "/val").text)["num_results"])
-                print("Error: get_val, valがリセットされました")
-                # print(num_val, len(data["data"]))
-                return
+    for entry in data["data"]:
+        # Message[id]が存在しない場合、Message[id]に格納
+        if num_val < int(entry['id']):
+            num_val = int(entry['id'])
+            mes = f"{entry['timestamp']} : {entry['val']}\n"
+            await Valchannel.send(mes)
+    # print(num_val, len(data["data"]))
+    if num_val > len(data["data"]):
+        num_val = int(json.loads(requests.get(url + "/val").text)["num_results"])
+        mes = "get_val, valがリセットされました"
+        print(f"Error: {mes}")
+        await Logchannel.send(makelog("Error", mes))
+        # print(num_val, len(data["data"]))
+        return
 
 @tasks.loop(seconds=10)
 async def get_notice():
@@ -159,27 +192,25 @@ async def get_notice():
         response = requests.get(url + "/notice")
         data = json.loads(response.text)
     except:
-        errmes = "Error: メッセージの取得に失敗しました"
-        for channel in bot.get_all_channels():
-            if int(channel.id) == int(noticechannelid):
-                await channel.send(errmes)
+        mes = "メッセージの取得に失敗しました. apiサーバーが起動しているか確認してください.notice"
+        await Noticechannel.send(makelog("Error", mes))
+        await Logchannel.send(makelog("Error", mes))
         return
-    for channel in bot.get_all_channels():
-        if int(channel.id) == int(noticechannelid):
-            if num_notice == len(data["data"]):
-                return
+    if num_notice == len(data["data"]):
+        return
+    else:
+        num_notice += 1
+        try:
+            if data["data"][num_notice - 1]["notice"] == 1:
+                await Noticechannel.send(f"```{data['data'][num_notice - 1]['timestamp']} : 水やり開始```")
+            elif data["data"][num_notice - 1]["notice"] == 0:
+                await Noticechannel.send(f"```{data['data'][num_notice - 1]['timestamp']} : 水やり終了```")
             else:
-                num_notice += 1
-                try:
-                    if data["data"][num_notice - 1]["notice"] == 1:
-                        await channel.send(f"```{data['data'][num_notice - 1]['timestamp']} : 水やり開始```")
-                    elif data["data"][num_notice - 1]["notice"] == 0:
-                        await channel.send(f"```{data['data'][num_notice - 1]['timestamp']} : 水やり終了```")
-                    else:
-                        await channel.send(f"```{data['data'][num_notice - 1]['timestamp']} : 水やりtimeout```")
-                except:
-                    print("Error: get_notice, noticeがリセットされました")
-                    num_notice = len(data["data"])
+                await Noticechannel.send(f"```{data['data'][num_notice - 1]['timestamp']} : 水やりtimeout```")
+        except:
+            mes = "get_notice, noticeがリセットされました"
+            await Logchannel.send(makelog("Error", mes))
+            num_notice = len(data["data"])
 
 @tasks.loop(seconds=60)
 # 指定時間に水やり指示を送信
@@ -200,13 +231,11 @@ async def post_flag():
                 try:
                     response = requests.post(flag_url, json=data)
                 except:
-                    for channel in bot.get_all_channels():
-                        if int(channel.id) == int(valchannelid):
-                            await channel.send(f"Error: 水やり指示の送信に失敗しました")
+                    await Valchannel.send(f"Error: 水やり指示の送信に失敗しました")
+                    await Logchannel.send(makelog("Error", "水やり指示の送信に失敗しました"))
                     return
-                for channel in bot.get_all_channels():
-                    if int(channel.id) == int(valchannelid):
-                        await channel.send(f"水やり指示を出しました")
+                await Valchannel.send(f"水やり指示を出しました")
+                await Logchannel.send(makelog("Info", "水やり指示を出しました"))
                 return
 
 get_val.start()
